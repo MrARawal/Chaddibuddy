a7e26cf915e1e4ac90f91bedd4be6460360ad958
MrARawal <141605240+MrARawal@users.noreply.github.com>
2023-10-05 02:13:22 +0530

    friends

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,21 +4,22 @@ import SearchBox from '../components/SearchBox';
 import './App.css'
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import robots from '../robots';
 
 class App extends Component{
 	constructor() {
 		super()
 		this.state = {
-			robots: [],
+			robots: robots,
 			searchfield: ''
 		}
 	}
 
-	componentDidMount(){
+/*	componentDidMount(){
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then(users => this.setState({ robots: users}));
-	}
+	}*/
 
 
 	onSearchChange = (event) => {
--- src/robots.js
+++ src/robots.js
@@ -1,72 +1,74 @@
-export const robots = [
+const robots = [
   {
     id: 1,
     name: 'Mohit Patil',
     username: 'MarathiMulga',
-    DOB: 'BirthRite: 11th May, 1997',
-    Phone: '+91 77382 12312'
+    DOB: 'BirthRite: 11th May',
+    Phone: '+91 ***** *****'
   },
   {
     id: 2,
     name: 'Kaushik Mehta',
     username: 'BusinessMunda',
-    DOB: 'BirthRite: 14th Dec, 1997',
-    Phone: '+91 89764 99998'
+    DOB: 'BirthRite: 14th December',
+    Phone: '+91 ***** *****'
   },
   {
     id: 3,
     name: 'Sadaf Alyani',
     username: 'Sadak',
-    DOB: 'BirthRite: 30th Dec, 1997',
-    Phone: '+91 99303 90781'
+    DOB: 'BirthRite: 30th December',
+    Phone: '+91 ***** *****'
   },
   {
     id: 4,
     name: 'Manisha Rawal',
     username: 'AnkitKiDulhan',
-    DOB: 'BirthRite: 18th Sept, 1998',
-    Phone: '+91 90716 72329'
+    DOB: 'BirthRite: 18th September',
+    Phone: '+91 ***** *****'
   },
   {
     id: 5,
     name: 'Vishal Kabra',
     username: 'LappuSaNRI',
-    DOB: 'BirthRite: 20th Oct, 1997',
-    Phone: '+1 (519) 722-3092 '
+    DOB: 'BirthRite: 20th October',
+    Phone: '+1 (***)*** **** '
   },
   {
     id: 6,
     name: 'Manan Kothari',
     username: 'Wannabe CA',
-    DOB: 'BirthRite: 6th Aug, 1998',
-    Phone: '+91 99677 68938'
+    DOB: 'BirthRite: 6th August',
+    Phone: '+91 ***** *****'
   },
   {
     id: 7,
     name: 'Tejas Singh',
     username: 'SleepyWhenever',
-    DOB: 'BirthRite: 22nd Dec, 1998',
-    Phone: '+91 77388 03992'
+    DOB: 'BirthRite: 22nd December',
+    Phone: '+91 ***** *****'
   },
   {
     id: 8,
     name: 'Vishwa Jain',
     username: 'Dautt',
-    DOB: 'BirthRite: 1st Apr, 1997',
-    Phone: '+91 98692 81241'
+    DOB: 'BirthRite: 1st April',
+    Phone: '+91 ***** *****'
   },
   {
     id: 9,
     name: 'Yash Shah',
     username: 'PhotoBmr',
-    DOB: 'BirthRite: 30th Mar, 1998',
-    Phone: '+91 80806 06787'
+    DOB: 'BirthRite: 30th March',
+    Phone: '+91 ***** *****'
   },
   {
     id: 10,
     name: 'Vandana Desai',
     username: 'NRIonDiet',
-    DOB: 'BirthRite: 21st Sept, 1997',
-    Phone: '+1 (437) 989-2797'
+    DOB: 'BirthRite: 21st September',
+    Phone: '+1 (***)*** ****'
   }
 ];
+
+export default robots;
